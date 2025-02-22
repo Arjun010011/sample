1ff26276e189300547c2c178f087c0ea6208bc85
arjun <arjun.m010011@gmail.com>
2025-02-22 15:52:48 +0530

    started creating footer for the landing page

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,7 +1,10 @@
+import { Logo } from "./logo";
+
 export const Footer = () => {
   return (
-    <div className=" flex items-center justify-center w-full p-6  z-50">
-      Footer
+    <div className=" flex items-center  w-full p-6  z-50">
+      <Logo />
+      <div className="ml-auto  w-full justify-between  md:justify-center"></div>
     </div>
   );
 };
