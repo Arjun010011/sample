e1dabe864a3da1079434fb8df96c96f9b974b6b3
arjun <arjun.m010011@gmail.com>
2025-02-28 19:35:09 +0530

    fixed some issues in build

--- components/spinner.tsx
+++ components/spinner.tsx
@@ -1,4 +1,4 @@
-import { Loader } from "lucide-react";
+import { Loader, type LucideProps } from "lucide-react";
 import { cva, type VariantProps } from "class-variance-authority";
 import { cn } from "@/lib/utils";
 
@@ -7,11 +7,7 @@ const spinnerVariant = cva("text-muted-foreground animate-spin", {
     size: {
       default: "h-4 w-4",
       sm: "h-2 w-2",
-<<<<<<< HEAD
-      lg: "h-6 2-6",
-=======
       lg: "h-6 w-6",
->>>>>>> 3b64a6940de12b829bdb830d94fd912f6696e1c8
       icons: "h-10 w-10",
     },
   },
@@ -20,16 +16,12 @@ const spinnerVariant = cva("text-muted-foreground animate-spin", {
   },
 });
 
-<<<<<<< HEAD
 interface SpinnerProps
-  extends Omit<React.ComponentProps<typeof Loader>, "size">,
+  extends Omit<LucideProps, "size">,
     VariantProps<typeof spinnerVariant> {}
 
-export const Spinner = ({ size }: SpinnerProps) => {
-=======
-interface spinnerProps extends VariantProps<typeof spinnerVariant> {}
-
-export const Spinner = ({ size }: spinnerProps) => {
->>>>>>> 3b64a6940de12b829bdb830d94fd912f6696e1c8
-  return <Loader className={cn(spinnerVariant({ size }))} />;
+export const Spinner = ({ size, className, ...props }: SpinnerProps) => {
+  return (
+    <Loader className={cn(spinnerVariant({ size }), className)} {...props} />
+  );
 };
