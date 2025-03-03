87be2db98ac25a5a390d7efca3708a388cd9c0de
arjun <arjun.m010011@gmail.com>
2025-03-03 21:54:57 +0530

    made few changes and updates on main page adding ui components

--- app/(main)/(routes)/documents/page.tsx
+++ app/(main)/(routes)/documents/page.tsx
@@ -1,5 +1,27 @@
+"use client";
+import Image from "next/image";
+import { useUser } from "@clerk/nextjs";
 const DocumentPage = () => {
-  return <div className="">this is a protected page</div>;
+  const { user } = useUser();
+  return (
+    <div className="h-full flex flex-col justify-center items-center space-y-4">
+      <Image
+        src="/dark_empty_box.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="dark:hidden"
+      />
+      <Image
+        src="/light_empty_box.png"
+        height="300"
+        width="300"
+        alt="empty"
+        className="hidden dark:block"
+      />
+      <h2 className="text-lg ">Welcome to {user?.firstName}&apos;s cooper</h2>
+    </div>
+  );
 };
 
 export default DocumentPage;
--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,17 +1,18 @@
 "use client";
 
 import { ChevronsLeft, MenuIcon } from "lucide-react";
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { useMediaQuery } from "usehooks-ts";
+import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
 const Navigation = () => {
   const isResizingRef = useRef(false);
-  // const pathName = usePathname();
+  const pathName = usePathname();
   const isMobile = useMediaQuery("(max-width:768px)");
   const sideBarRef = useRef<ElementRef<"aside">>(null);
   const navbarRef = useRef<ElementRef<"div">>(null);
-  const [isResetting] = useState(false);
-  // const [isCollapsed, setIsCollapsed] = useState(isMobile);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
 
   const handleMouseDown = (
     event: React.MouseEvent<HTMLDivElement, MouseEvent>
@@ -22,6 +23,7 @@ const Navigation = () => {
     document.addEventListener("mousemove", handleMouseMove);
     document.addEventListener("mouseup", handleMouseUp);
   };
+
   const handleMouseMove = (event: MouseEvent) => {
     if (!isResizingRef.current) return;
     let newWidth = event.clientX;
@@ -41,17 +43,54 @@ const Navigation = () => {
     document.removeEventListener("mousemove", handleMouseUp);
     document.removeEventListener("mouseup", handleMouseUp);
   };
+
+  const resetWidth = () => {
+    if (sideBarRef.current && navbarRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(false);
+
+      sideBarRef.current.style.width = isMobile ? "100%" : "240px";
+      navbarRef.current.style.setProperty("width", isMobile ? "100%" : "240px");
+      setTimeout(() => setIsResetting(false), 500);
+    }
+  };
+
+  const collapse = () => {
+    if (sideBarRef.current && navbarRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+      sideBarRef.current.style.width = "0";
+      navbarRef.current.style.setProperty("width", "100%");
+      navbarRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 500);
+    }
+  };
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    } else {
+      resetWidth();
+    }
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) {
+      collapse();
+    }
+  }, [pathName, isMobile]);
   return (
     <>
       <aside
         ref={sideBarRef}
         className={cn(
           "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-99999",
-          isResetting && "transition-all ease-in-out  duration-300",
+          isResetting && "transition-all ease-in-out  duration-500",
           isMobile && "w-0"
         )}
       >
         <div
+          onClick={collapse}
           className={cn(
             "h-5 w-5 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-3 opacity-0 group-hover/sidebar:opacity-100 transition",
             isMobile && "opacity-100"
@@ -69,6 +108,7 @@ const Navigation = () => {
         {/* this is the sidebar */}
         <div
           onMouseDown={handleMouseDown}
+          onClick={resetWidth}
           className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10  right-0 top-0"
         />
       </aside>
@@ -76,13 +116,17 @@ const Navigation = () => {
         ref={navbarRef}
         className={cn(
           "absolute top-0 z-[99999] left-60 w-[calc(100% - 240px)]",
-          isResetting && "transtion-all ease-in-out duration-300 ",
+          isResetting && "transtion-all ease-in-out duration-500 ",
           isMobile && "left-0 w-full"
         )}
       >
         <nav className="bg-transparent px-3 py-2 w-full">
-          {isMobile && (
-            <MenuIcon className="h-6 w-6 text-muted-foreground" role="button" />
+          {isCollapsed && (
+            <MenuIcon
+              onClick={resetWidth}
+              className="h-6 w-6 text-muted-foreground"
+              role="button"
+            />
           )}
         </nav>
       </div>
